ea9890f0443151d36ae2c29c657d148e5027d23a
444101792 <444101792@student.ksu.edu.sa>
2025-12-03 09:00:08 +0300

    fixed some issues with the home page

--- SaudiEstate/main/views.py
+++ SaudiEstate/main/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from properties.models import Property
 
-def home(request):
-    return render(request, 'main/home.html')
+def home(request):    
+    properties = Property.objects.all().order_by('?')
+
+    
+    
+    if request.user.is_authenticated:
+       
+        user_city = getattr(request.user, 'city', None)
+        
+        if user_city:
+            city_properties = Property.objects.filter(city__icontains=user_city).order_by('-created_at')
+            if city_properties.exists():
+                properties = city_properties
+    featured_properties = properties[:3]
+
+    context = {
+        'properties': featured_properties
+    }
+    return render(request, 'main/home.html', context)
 
 
 def all_properties(request):
